571e86ffa2d5bf7f7271b2943c8f217e34e30274
harshraythattha <hpr5@njit.edu>
2020-03-02 13:56:01 -0500

    Mean.js done

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const Calculator = require('./MathOperations');
+const banana = require('./mean');
 a = 1;
 b = 2;
 
 sum = Calculator.sum(a,b);
 product = Calculator.product(a,b);
 quotient = Calculator.quotient(a,b);
+mean = banana([1,2,3,4,5]);
 
 console.log("The sum is: " + sum);
 console.log("The product is: " + product);
 console.log("The quotient is: " + quotient);
+console.log("The mean is: " + mean);
